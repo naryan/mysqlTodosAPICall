2711f9c909192bf8f73a8d03112064ccf951750d
Narayan <aayanpoudel@gmail.com>
2020-04-27 11:50:13 -0700

    created frontend

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 app.use(routes);
 
 app.listen(PORT);
